4736a723de70fb0fc08c40da53354e5728cc0a8f
mila.pro <lpromyslova@mail.ru>
2018-04-17 00:40:57 +0300

    get list from http request

--- src/ApiUsageEx.elm
+++ src/ApiUsageEx.elm
@@ -1,5 +1,5 @@
-import Html exposing (Html, div, button, text, img)
-import Html.Attributes exposing (style, src)
+import Html exposing (Html, div, button, text, img, select, option)
+import Html.Attributes exposing (style, src, selected)
 import Html.Events exposing (onClick)
 import Json.Decode as Decode
 import Http
@@ -16,18 +16,20 @@ main =
 
 type alias Model =
   {
-    url : String
+    url : String,
+    breeds : List String
   }
 
 init : (Model, Cmd Msg)
 init =
-  (Model "", getRandomImg)
+  (Model "" [], getRandomImg)
 
 -- UPDATE
 
 type Msg
   = Generate
   | NewImg (Result Http.Error String)
+  | NewBreedsList (Result Http.Error (List String))
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -39,6 +41,12 @@ update msg model =
       ( { model | url = newUrl }, Cmd.none)
 
     NewImg (Err _) ->
+      (model, Cmd.none)
+
+    NewBreedsList (Ok newList) ->
+      ( { model | breeds = newList }, Cmd.none)
+
+    NewBreedsList (Err _) ->
       (model, Cmd.none)
 
 -- SUBSCRIPTIONS
@@ -53,6 +61,9 @@ view : Model -> Html Msg
 view model =
   div [ containerStyle ]
     [
+      select [ selectStyle ] [
+        option [] [ text "All" ]
+      ],
       img [ imgStyle, src model.url ] [],
       button [ btnStyle, onClick Generate ] [ text "Generate" ]
     ]
@@ -69,7 +80,8 @@ imgStyle =
   style
     [
       ("height", "300px"),
-      ("margin-bottom", "30px")
+      ("display", "block"),
+      ("margin", "30px auto")
     ]
 
 btnStyle =
@@ -88,6 +100,12 @@ btnStyle =
       ("border-bottom", "7px solid #512DA8")
     ]
 
+selectStyle =
+  style
+    [
+      ("outline", "none")
+    ]
+
 -- HTTP
 
 getRandomImg : Cmd Msg
@@ -96,10 +114,25 @@ getRandomImg =
     url = "https://dog.ceo/api/breeds/image/random"
 
     request =
-      Http.get url decodeImgUrl
+      Http.get url decodeImgData
   in
     Http.send NewImg request
 
-decodeImgUrl : Decode.Decoder String
-decodeImgUrl =
+decodeImgData : Decode.Decoder String
+decodeImgData =
   Decode.at ["message"] Decode.string
+
+
+getBreedsList : Cmd Msg
+getBreedsList =
+  let
+    url = "https://dog.ceo/api/breeds/list"
+
+    request =
+      Http.get url decodeBreedsData
+  in
+    Http.send NewBreedsList request
+
+decodeBreedsData : Decode.Decoder (List String)
+decodeBreedsData =
+  Decode.at ["message"] (Decode.list Decode.string)
